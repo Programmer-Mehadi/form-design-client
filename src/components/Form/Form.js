b03fc1180c1da50efd294924e2857ab19f15a088
Programmer-Mehadi <mehadi.developer@gmail.com>
2023-01-19 22:17:26 +0600

    form-done

---
+++ src/components/Form/Form.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import EmptyState from '../../asset/banner/Empty State.svg';
+const Form = () => {
+    return (
+        <div className='px-4  py-[50px]  '>
+            <div className='border-[3px] rounded-[24px]  border-[#0370DD] grid grid-cols-1 md:grid-cols-5'>
+                <div className='md:col-span-2 pt-[75px] pb-4 pl-[30px]  md:pl-[58px] md:border-r md:border-slate-600 order-2 md:order-1'>
+                    <div className='flex flex-col gap-4 mr-10'>
+                        <h2 className='text-[30px] md:text-[42px] font-extrabold'>Education<span className='text-red-600'>*</span></h2>
+                        <div className="form-control w-full ">
+                            <label className="label">
+                                <span className="label-text text-[14px] md:text-[18px] text-[#302B27] font-medium">Year of passing<span className='text-red-600'>*</span></span>
+                            </label>
+                            <input type="text" placeholder="2024" className="input input-bordered border-[1.5px] border-slate-500  w-[100%] " />
+                            <label className="label flex justify-end">
+                                <span className="label-text-alt font-medium text-[14px] md:text-[18px] text-[#707784]">0/100</span>
+                            </label>
+                        </div>
+                        <div className="form-control w-full ">
+                            <label className="label">
+                                <span className="label-text text-[14px] md:text-[18px] text-[#302B27] font-medium">Region<span className='text-red-600'>*</span></span>
+                            </label>
+                            <input type="text" placeholder="Jalandhar" className="input input-bordered border-[1.5px] border-slate-500  w-[100%] " />
+                            <label className="label flex justify-end">
+                                <span className="label-text-alt font-medium text-[14px] md:text-[18px] text-[#707784]">0/100</span>
+                            </label>
+                        </div>
+                        <div className="form-control w-full ">
+                            <label className="label">
+                                <span className="label-text text-[14px] md:text-[18px] text-[#302B27] font-medium">College Name<span className='text-red-600'>*</span></span>
+                            </label>
+                            <input type="text" placeholder="NIT Jalandhar" className="input input-bordered border-[1.5px] border-slate-500  w-[100%] " />
+                            <label className="label flex justify-end">
+                                <span className="label-text-alt font-medium text-[14px] md:text-[18px] text-[#707784]">0/100</span>
+                            </label>
+                        </div>
+                        <div className="form-control w-full ">
+                            <label className="label">
+                                <span className="label-text text-[14px] md:text-[18px] text-[#302B27] font-medium">Describe College<span className='text-red-600'>*</span></span>
+                                <span className='font-medium text-[14px] md:text-[18px] text-[#0370DD]'>Need help<span className='ml-1'>?</span> </span>
+                            </label>
+                            <textarea placeholder="NITJ is a college of national importance and students can get admission into this college through the JEE mains entrance exam and then the JoSSA counselling ...." className="input input-bordered border-[1.5px] border-slate-500  w-[100%] min-h-[220px]  md:min-h-[165px] py-3" />
+                            <label className="label flex justify-end">
+                                <span className="label-text-alt font-medium text-[14px] md:text-[18px] text-[#707784]">0/200</span>
+                            </label>
+                        </div>
+                        <div className="form-control w-full">
+                            <button className='btn btn-primary bg-[#0370DD] rounded-[8px] border-0 px-[40px] py-[16px] w-fit normal-case text-[20px]'>Generate</button>
+                        </div>
+                    </div>
+                </div>
+                <div className='md:col-span-3 pt-[75px] pb-4 px-[20px] order-1 md:order-2'>
+                    <img src={EmptyState} alt="" />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Form;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import ellipse1 from '../../asset/logo/Ellipse 1.svg'
+import vector from '../../asset/logo/Vector.svg'
+
+const Navbar = () => {
+    return (
+        <div className="navbar px-4  py-4 flex">
+            <div className="navbar-start">
+
+                <a className="flex ">
+                    <img src={ellipse1} alt="" className='mr-4 w-[40px]  md:w-[60px] md:h-[60px]' />
+                    <img src={vector} alt="" className='w-[80px] md:w-[110px]' />
+                </a>
+            </div>
+            <div className="navbar-center hidden lg:flex ">
+                <ul className="menu menu-horizontal px-1 text-[18px] font-medium">
+                    <li><a>Pricing</a></li>
+                    <li><a>Features</a></li>
+                    <li><a>Customers</a></li>
+                    <li><a>Contact Us</a></li>
+                </ul>
+            </div>
+            <div className="navbar-end hidden lg:flex ">
+                <button className='tracking-wider btn btn-primary uppercase bg-[#0370DD] rounded-[16px] border-0 px-8'>Login/Signup</button>
+            </div>
+            <div className='navbar-end lg:hidden'>
+                <div className="dropdown">
+                    <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </label>
+                    <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52 ml-[-200px]">
+                        <li><a>Pricing</a></li>
+                        <li><a>Features</a></li>
+                        <li><a>Customers</a></li>
+                        <li><a>Contact Us</a></li>
+                        <button className='tracking-wider btn btn-primary uppercase bg-[#0370DD] rounded-[16px] border-0 px-8'>Login/Signup</button>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
